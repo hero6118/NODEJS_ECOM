3e82f38d81cbae7bdf75a36f550f1600c230b122
thuan <thuannguyenTHCST4@gmail.com>
2023-03-26 00:24:25 +0700

    t2h21 commit oh right with the show number of the products

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { useEffect, useReducer } from 'react';
+import { useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -9,48 +10,58 @@ import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/Button';
 import Rating from '../components/Rating';
 import { Helmet } from 'react-helmet-async';
+
 import { getError } from '../utils';
+import { Store } from '../Store';
+import LoadingBox from './LoadingBox';
+import MessageBox from './MessageBox';
+
 const reducer = (state, action) => {
-    switch (action.type) {
-      case 'FETCH_REQUEST':
-        return { ...state, loading: true };
-      case 'FETCH_SUCCESS':
-        return { ...state, product: action.payload, loading: false };
-      case 'FETCH_FAIL':
-        return { ...state, loading: false, error: action.payload };
-      default:
-        return state;
-    }
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, product: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+function ProductScreen() {
+  const params = useParams();
+  const { slug } = params;
+  const [{ loading, error, product }, dispatch] = useReducer(reducer, {
+    product: [],
+    loading: true,
+    error: '',
+  });
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get(`/api/products/slug/${slug}`);
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
+      }
+    };
+    fetchData();
+  }, [slug]);
+
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const addToCartHandler = () => {
+    ctxDispatch({
+      type: 'CART_ADD_ITEM',
+      payload: { ...product, quantity: 1 },
+    });
   };
-function ProductScreen(){
-    const params = useParams();
-    const{slug} = params;
-    const [{ loading, error, product }, dispatch] = useReducer(reducer, {
-        product: [],
-         loading: true,
-         error: '',
-     });
-    // const[products, setProducts] = useState([]);
-     useEffect(( )=>{
-             const fetchData = async () =>{
-                 dispatch({type: 'FETCH_REQUEST'});
-                 try{
-                     const result = await axios.get(`/api/products/slug/${slug}`);
-                     dispatch({type:'FETCH_SUCCESS', payload: result.data});
-                 } catch(err){
-                         dispatch({type: 'FETCH_FAIL', payload: getError(err) });
-                 }
- 
-                 
-                 // setProducts(result.data);
-             };
-             fetchData();
-     },[slug]);
-    return(
-        loading? <div>Loading...</div>: error? <div>{error}</div>
-        :
-        <div>
-      <h1>{slug}</h1>
+  return loading ? (
+    <LoadingBox />
+  ) : error ? (
+    <MessageBox variant="danger">{error}</MessageBox>
+  ) : (
+    <div>
       <Row>
         <Col md={6}>
           <img
@@ -62,7 +73,9 @@ function ProductScreen(){
         <Col md={3}>
           <ListGroup variant="flush">
             <ListGroup.Item>
-             
+              <Helmet>
+                <title>{product.name}</title>
+              </Helmet>
               <h1>{product.name}</h1>
             </ListGroup.Item>
             <ListGroup.Item>
@@ -93,18 +106,20 @@ function ProductScreen(){
                     <Col>Status:</Col>
                     <Col>
                       {product.countInStock > 0 ? (
-                        <Badge bg="success">Còn Hàng</Badge>
+                        <Badge bg="success">In Stock</Badge>
                       ) : (
-                        <Badge bg="danger">hết hàng</Badge>
+                        <Badge bg="danger">Unavailable</Badge>
                       )}
                     </Col>
                   </Row>
                 </ListGroup.Item>
-
                 {product.countInStock > 0 && (
                   <ListGroup.Item>
                     <div className="d-grid">
-                      <Button variant="primary">Thêm vào giỏ hàng</Button>
+                 
+                      <Button onClick={addToCartHandler} variant="primary">
+                        Add to Cart
+                      </Button>
                     </div>
                   </ListGroup.Item>
                 )}
