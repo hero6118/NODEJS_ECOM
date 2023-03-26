1fd241e96dcf5cfded46f24c148bd77ba0024d58
thuan <thuannguyenTHCST4@gmail.com>
2023-03-26 21:17:56 +0700

    c19 testdb

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { useContext } from 'react';
 import { Store } from './Store';
 import CartScreen from './screens/CartScreen';
+import SigninScreen from './screens/SigninScreen';
 function App() {
   const { state } = useContext(Store);
   const { cart } = state;
@@ -46,6 +47,7 @@ function App() {
         <Route path="/product/:slug" element={<ProductScreen/>} />
         <Route path="/cart" element={<CartScreen />} />
         <Route path="/" element={<HomeScreen/>}/>
+        <Route path="/signin" element={<SigninScreen />} />
        </Routes>
        </Container>
         </main>
