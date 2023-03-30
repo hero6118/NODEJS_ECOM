62ee41bc1b583c7f3bbefe7fa3eebe6e5a4d05ea
thuan <thuannguyenTHCST4@gmail.com>
2023-03-30 15:27:22 +0700

    sign inss:errorr

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
 const data={
+    users: [
+        {
+          name: 'thuan',
+          email: 'admin@example.com',
+          password: bcrypt.hashSync('123456'),
+          isAdmin: true,
+        },
+        {
+          name: 'duy',
+          email: 'user@example.com',
+          password: bcrypt.hashSync('123456'),
+          isAdmin: false,
+        },
+      ],
+
     products: [
         {
            // _id:'1',
